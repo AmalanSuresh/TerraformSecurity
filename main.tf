880d44dbe3a391776bf878ed396addaecc6b0bd1
AmalanSuresh <109796428+AmalanSuresh@users.noreply.github.com>
2025-11-11 12:34:36 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Step 1: Create a Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "amalan-sg"
+  }
+}
+
+# Step 2: Launch EC2 Instance
+resource "aws_instance" "amalan_ec2" {
+  ami           = "ami-0844f359572fd681d" # for eu-north-1
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "amalan-secure-server"
+  }
+}
